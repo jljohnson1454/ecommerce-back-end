35280266eb67683eb78c513511281ff59d16c60d
Ghidorah <jljohnson1454@gmail.com>
2021-11-06 10:22:59 -0500

    added product tag model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
   Category.findAll({
     include: {
       model: Product,
-      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      //attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }
   })
   .then(dbCategoryData => res.json(dbCategoryData))
